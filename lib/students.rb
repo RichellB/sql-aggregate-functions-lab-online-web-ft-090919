ffab9fe4964a16a6fc15af9da7e6e64cb11779f6
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-08 04:22:47 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -4,8 +4,8 @@ def highest_student_gpa
   "SELECT MAX(gpa) FROM students;"
 end
 
-def 
-  "SELECT MIN(gpa) FROM students;"
+def lowest_student_gpa
+   "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
